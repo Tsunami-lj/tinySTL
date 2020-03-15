b40d33009ce695e728c9d9896a9490829b91ce38
tsunami_lj <727155384@qq.com>
2020-03-15 14:48:31 +0800

    update allocator

--- allocator.h
+++ allocator.h
@@ -1,3 +1,53 @@
 #ifndef MYSTL_ALLOCATOR_H_
 #define MYSTL_ALLOCATOR_H_
 
+#include "alloc.h"
+
+namespace mystl {
+
+template <class T>
+class allocator {
+    typedef alloc Alloc;
+public:
+    typedef size_t      size_type;
+    typedef ptrdiff_t   difference_type;
+    typedef T*          pointer;
+    typedef const T*    const_pointer;
+    typedef T&          reference;
+    typedef const T&    const_reference;
+    typedef T           value_type;
+public:
+    T* allocate() {
+        return static_cast<T*>(Alloc::allocate(sizeof(T)));
+    }
+    
+    T* allocate(size_type n, const void* = 0) {
+        return !n ? 0 : static_cast<T*>(Alloc::allocate(n * sizeof(T)));
+    }
+    
+    void deallocate(pointer p) {
+           Alloc::deallocate(p, sizeof(T));
+    }
+    
+    void deallocate(pointer p, size_type n) {
+        Alloc::deallocate(p, n * sizeof(T));
+    }
+    
+    void construct(pointer p) {
+        mystl::construct(p);
+    }
+    
+    void construct(pointer p, const T& value) {
+        mystl::construct(p, value);
+    }
+    
+    void destroy(pointer p) {
+        mystl::destroy(p);
+    }
+    
+    void destroy(T* first, T* last) {
+           mystl::destroy(first, last);
+       }
+};
+}
+#endif
