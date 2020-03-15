031bb766d844a0c994a6440ac18e313b2e2a24dc
tsunami_lj <727155384@qq.com>
2020-03-15 15:18:34 +0800

    update iterator.h

--- iterator.h
+++ iterator.h
@@ -13,7 +13,56 @@ struct forward_iterator_tag : public input_ietrator_tag {};
 struct bidirectional_iterator_tag : public forward_iterator_tag {};
 struct random_access_iterator_tag : public bidirectional_iterator_tag {};
 
-//迭代器模板
+//input
+template <class T, class Distance>
+struct input_ietrator {
+    typedef input_ietrator_tag  iterator_category;
+    typedef T                   value_type;
+    typedef Distance            difference_type;
+    typedef T*                  pointer;
+    typedef T&                  reference;
+};
+
+//output
+struct output_ietrator {
+    typedef output_ietrator_tag     iterator_category;
+    typedef void                    value_type;
+    typedef void                    difference_type;
+    typedef void                    pointer;
+    typedef void                    reference;
+};
+
+//forward
+template <class T, class Distance>
+struct forward_ietrator {
+    typedef forward_ietrator_tag    iterator_category;
+    typedef T                       value_type;
+    typedef Distance                difference_type;
+    typedef T*                      pointer;
+    typedef T&                      reference;
+};
+
+//bidirectional
+template <class T, class Distance>
+struct bidirectional_ietrator {
+    typedef bidirectional_ietrator_tag      iterator_category;
+    typedef T                               value_type;
+    typedef Distance                        difference_type;
+    typedef T*                              pointer;
+    typedef T&                              reference;
+};
+
+//random_access
+template <class T, class Distance>
+struct random_access_ietrator {
+    typedef random_access_ietrator_tag      iterator_category;
+    typedef T                               value_type;
+    typedef Distance                        difference_type;
+    typedef T*                              pointer;
+    typedef T&                              reference;
+};
+
+//iterator
 template <class Category, class T, class Distance = ptrdiff_t,
           class Pointer = T*, class Reference = T&>
 struct iterator {
@@ -54,15 +103,30 @@ struct iterator_traits <const T*> {
     typedef const T&                       reference;
 };
 
-//获取迭代器类别
+//获取迭代器category
 template <class Iter>
 inline typename iterator_traits<Iter>::iterator_category
-__iteraor_category(const Iter&)
+iteraor_category(const Iter&)
 {
     typedef typename iterator_traits<Iter>::iterator_category Category;
     return Category();
 }
 
+//获取迭代器 distance type
+template <class Iter>
+inline typename iterator_traits<Iter>::difference_type *
+distance_type(const Iter&)
+{
+    return static_cast<typename iterator_traits<Iter>::difference_type *>(0);
+}
 
+//获取迭代器 value type
+template<class Iter>
+inline typename iterator_traits<Iter>::value_type *
+value_type(const Iter&)
+{
+    return static_cast<typename iterator_traits<Iter>::value_type *>(0);
+}
 
 }
+#endif
