2dce15b4c089554f3abfb6ca1c083e7c6a09807e
tsunami_lj <727155384@qq.com>
2020-03-21 17:40:44 +0800

    create heap.h

---
+++ heap.h
@@ -0,0 +1,93 @@
+#ifndef MYSTL_HEAP_H_
+#define MYSTL_HEAP_H_
+//heap 操作的四个方法：push_heap, pop_heap, make_heap, sort_heap.
+
+//将元素插入head尾部之后，向上调整，构建最大堆
+template <class RandomAccessIterator>
+inline void push_heap(RandomAccessIterator first, RandomAccessIterator last) {
+    __push_heap_aux(first, last, distance_type(first), value_type(first));
+}
+
+template <class RandomAccessIterator, class Distance, class T>
+inline void __push_heap_aux(RandomAccessIterator first,
+                            RandomAccessIterator last, Distance*, T*)
+{
+    __push_heap(first, Distance((last - first) - 1), Distance(0), T(*(last - 1)));
+}
+
+template <class RandomAccessIterator, class Distance, class T>
+void __push_heap(RandomAccessIterator first, Distance holeIndex,
+                 Distance topIndex, T value) {
+    Distance parent = (holeIndex - 1) / 2;
+    while (holeIndex > topIndex && *(first + parent) < value) {
+        *(first + holeIndex) = *(first + parent);
+        holeIndex = parent;
+        parent = (holeIndex - 1) / 2;
+    }
+    *(first + holeIndex) = value;
+}
+
+//弹出堆顶元素，并做一次向下调整，之后将尾元素做一次向上调整。
+template <class RandomAccessIterator>
+inline void pop_heap(RandomAccessIterator first,
+                               RandomAccessIterator last) {
+    __pop_heap_aux(first, last, value_type(first));
+};
+
+template <class RandomAccessIterator, class T>
+inline void __pop_heap_aux(RandomAccessIterator first, RandomAccessIterator last, T*) {
+    __pop_heap(first, last - 1, last - 1, T(*(last - 1)), distance_type(first));
+}
+
+template <class RandomAccessIterator, class T, class Distance>
+inline void __pop_heap(RandomAccessIterator first, RandomAccessIterator last,
+                       RandomAccessIterator result, T value, Distance*) {
+    *result = *first;
+    __adjust_heap(first, Distance(0), Distance(last - first), value);
+}
+
+template <class RandomAccessIterator, class Distance, class T>
+void __adjust_heap(RandomAccessIterator first, Distance holeIndex, Distance len, T value) {
+    Distance topIndex = holeIndex;
+    Distance secondChild = 2 * holeIndex + 2;
+    while (secondChild < len) {
+        if (*(first + secondChild) < *(first + secondChild - 1))
+            --secondChild;
+        *(first + holeIndex) = *(first + secondChild);
+        holeIndex = secondChild;
+        secondChild = 2  * (secondChild + 1);
+    }
+    if (secondChild == len) {
+        *(first + holeIndex) = *(first + secondChild - 1);
+        holeIndex = secondChild - 1;
+    }
+    __push_heap(first, holeIndex, topIndex, value);
+}
+
+template <class RandomAccessIterator>
+inline void make_heap(RandomAccessIterator first, RandomAccessIterator last) {
+    __make_heap(first, last, value_type(first), distance_type(first));
+}
+
+template <class RandomAccessIterator. class T,  class Distance>
+void __make_heap(RandomAccessIterator first, RandomAccessIterator last, T*, Distance*) {
+    if (last - first < 2)
+        return ;
+    Distance len = last - first;
+    Distance parent = (len - 2) / 2;
+    //从 (len - 2) / 2 -> 0调整最大堆
+    while (true) {
+        __adjust_heap(first, parent, len, T(*(first + parent)));
+        if (parent == 0)
+            return;
+        parent--;
+    }
+}
+
+template <class RandomAccessIterator>
+void sort_heap(RandomAccessIterator first, RandomAccessIterator last) {
+    while (last - first > 1)
+        pop_heap(first, last--);
+}
+
+#endif
